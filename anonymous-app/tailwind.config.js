be4551fac817f822d209fcb22cd7e7498c341d33
JGokulnath <gokul011002@gmail.com>
2024-01-23 17:07:05 +0530

    tailwind.js module modified

--- anonymous-app/tailwind.config.js
+++ anonymous-app/tailwind.config.js
@@ -1,13 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
   content: ["./src/**/*.{html,js}"],
-=======
-  content: [],
->>>>>>> 0776c35727e9c56072ec2e6a597421a8e7a7d308
+
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
